c10470d42eba7a413c4a3ce772099101fb5e630c
danic999 <lkordic1999@gmail.com>
2023-07-11 09:51:04 +0200

    structure update

--- wp-content/themes/stellar/functions.php
+++ wp-content/themes/stellar/functions.php
@@ -1,26 +1,7 @@
-<?php   
+<?php
 
-function load_css()
-{
-        wp_register_style('stellar', get_template_directory_uri() . '/assets/css/style.css',array(),false,'all');
-        wp_enqueue_style('stellar');
+foreach ( glob( get_theme_file_path() . '/inc/*.php' ) as $file ) {
 
+require_once $file;
 }
-add_action('wp_enqueue_scripts','load_css');
-
-       
-
-function load_js()
-{
-
-    wp_register_script('stellar', get_template_directory_uri() . '/assets/js/main.js','jquery',false,true);
-    wp_enqueue_script('stellar');
-
-
-}
-add_action('wp_enqueue_scripts','load_js')
-
-
-
-
 ?>
---
+++ wp-content/themes/stellar/inc/scripts.php
@@ -0,0 +1,26 @@
+<?php   
+
+function load_css()
+{
+        wp_register_style('stellar', get_template_directory_uri() . '/assets/src/css/style.css',array(),false,'all');
+        wp_enqueue_style('stellar');
+
+}
+add_action('wp_enqueue_scripts','load_css');
+
+       
+
+function load_js()
+{
+
+    wp_register_script('stellar', get_template_directory_uri() . '/assets/src/js/main.js','jquery',false,true);
+    wp_enqueue_script('stellar');
+
+
+}
+add_action('wp_enqueue_scripts','load_js')
+
+
+
+
+?>

323a5ff5179ad29c3725d4bf9159dc35cb4918c7
danic999 <lkordic1999@gmail.com>
2023-08-28 11:08:36 +0200

    header

--- wp-content/themes/stellar/dist/app.js
+++ wp-content/themes/stellar/dist/app.js
@@ -7,7 +7,18 @@
   \******************************/
 /***/ (() => {
 
-alert('hello world');
+document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', function () {
+    var header = document.querySelector('.header');
+    if (header) {
+      if (window.scrollY > 0) {
+        header.style.backgroundColor = '#111926';
+      } else {
+        header.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+      }
+    }
+  });
+});
 
 /***/ }),
 

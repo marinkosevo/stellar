0a527bb43499e4ea6d3cf2715ef7e8f17ac8c398
danic999 <lkordic1999@gmail.com>
2023-08-28 16:15:02 +0200

    header update

--- wp-content/themes/stellar/assets/src/js/app.js
+++ wp-content/themes/stellar/assets/src/js/app.js
@@ -13,3 +13,4 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+console.log('hej')
--- wp-content/themes/stellar/dist/app.js
+++ wp-content/themes/stellar/dist/app.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+console.log('hej');
 
 /***/ }),
 

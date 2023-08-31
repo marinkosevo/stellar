affc1d71fc72730f65262034b09c2f03cd32881e
danic999 <lkordic1999@gmail.com>
2023-08-31 14:32:37 +0200

    formatiranje

--- wp-content/themes/stellar/assets/src/js/app.js
+++ wp-content/themes/stellar/assets/src/js/app.js
@@ -1,26 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var header = document.getElementsByClassName("header")[0];
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', function() {
-        var header = document.getElementsByClassName('header')[0];
-
-        if (header) {
-            if (window.scrollY > 0) {
-                header.style.backgroundColor = '#111926';
-            } else {
-                header.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
-            }
-        }
-    });
-});
-var menu= document.getElementsByClassName('header__nav')[0];
-var toggle= document.getElementById('toggle-menu');
-toggle.addEventListener('click',()=> {
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-    } else {
-        menu.classList.add('active');
-        menu.style= backgroundColor="black"
+    if (header) {
+      if (window.scrollY > 0) {
+        header.style.backgroundColor = "#111926";
+      } else {
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+      }
     }
-})
-console.log(menu)
+  });
+});
+var menu = document.getElementsByClassName("header__nav")[0];
+var toggle = document.getElementById("toggle-menu");
+toggle.addEventListener("click", () => {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+  } else {
+    menu.classList.add("active");
+    menu.style = backgroundColor = "black";
+  }
+});
+console.log(menu);
--- wp-content/themes/stellar/dist/app.js
+++ wp-content/themes/stellar/dist/app.js
@@ -7,25 +7,25 @@
   \******************************/
 /***/ (() => {
 
-document.addEventListener('DOMContentLoaded', function () {
-  window.addEventListener('scroll', function () {
-    var header = document.getElementsByClassName('header')[0];
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var header = document.getElementsByClassName("header")[0];
     if (header) {
       if (window.scrollY > 0) {
-        header.style.backgroundColor = '#111926';
+        header.style.backgroundColor = "#111926";
       } else {
-        header.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
       }
     }
   });
 });
-var menu = document.getElementsByClassName('header__nav')[0];
-var toggle = document.getElementById('toggle-menu');
-toggle.addEventListener('click', function () {
-  if (menu.classList.contains('active')) {
-    menu.classList.remove('active');
+var menu = document.getElementsByClassName("header__nav")[0];
+var toggle = document.getElementById("toggle-menu");
+toggle.addEventListener("click", function () {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
   } else {
-    menu.classList.add('active');
+    menu.classList.add("active");
     menu.style = backgroundColor = "black";
   }
 });

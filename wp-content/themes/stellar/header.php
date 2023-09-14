55f405b1d59880a98383dcb1b716f24b11dd44fd
danic999 <lkordic1999@gmail.com>
2023-09-14 10:45:09 +0200

    prepravke

--- wp-content/themes/stellar/header.php
+++ wp-content/themes/stellar/header.php
@@ -14,7 +14,7 @@
 
     <header>
         <div class="header">
-            <div class="header__wrapper max-w-[1200px] w-full mx-auto flex justify-between sm:gap-3 pl-20 pr-32 py-5">
+            <div class="header__wrapper max-w-[1200px] justify-items-start w-full mx-auto flex justify-between sm:gap-3 pl-20 pr-32 py-5">
                 <div class="header__mobile-menu sm:flex md:hidden" id="toggle-menu">
                     <img class="max-w-fit" src="<?php bloginfo('template_directory'); ?>/assets/images/menu-icon.png" alt="">
                 </div>
@@ -22,12 +22,12 @@
                     <img class="max-w-fit" src="<?php bloginfo('template_directory'); ?>/assets/images/nectar_logo.svg" alt="" srcset="">
                 </div>
                 <div class="header__nav-menu  ml-14 mr-24 hidden md:block">
-                    <nav class="nav-menu flex-row justify-between place-items-baseline	">
+                    <nav class="nav-menu flex-row justify-between 	">
                         <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
                     </nav>
                 </div>
                 <div class="header__nav-help hidden md:block ">
-                    <nav class="nav-menu flex-row justify-between place-items-baseline	">
+                    <nav class="nav-menu flex-row justify-between ">
                         <?php wp_nav_menu(array('theme_location' => 'help-menu')); ?>
                     </nav>
                 </div>

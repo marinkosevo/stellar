1895b928afa97fef299ebdd23fed514aa46219c6
danic999 <lkordic1999@gmail.com>
2023-09-15 09:29:30 +0200

    header vol2

--- wp-content/themes/stellar/header.php
+++ wp-content/themes/stellar/header.php
@@ -14,7 +14,7 @@
 
     <header>
         <div class="header">
-            <div class="header__wrapper max-w-[1280px] w-full mx-auto flex justify-between sm:gap-3  py-5">
+            <div class="header__wrapper max-w-[1280px] w-full mx-auto flex justify-evenly sm:gap-3 text-base py-5">
                 <div class="header__mobile-menu sm:flex md:hidden" id="toggle-menu">
                     <img class="max-w-fit" src="<?php bloginfo('template_directory'); ?>/assets/images/menu-icon.png" alt="">
                 </div>
@@ -22,12 +22,12 @@
                     <img class="max-w-fit" src="<?php bloginfo('template_directory'); ?>/assets/images/nectar_logo.svg" alt="" srcset="">
                 </div>
                 <div class="header__nav-menu  ml-14 mr-24 hidden md:block">
-                    <nav class="nav-menu flex-row justify-between 	">
+                    <nav class="nav-menu flex-row justify-evenly  text-blue-700 [&_ul]:flex [&_ul]:gap-[32px] [&_li:last-child]:text-red-500">
                         <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
                     </nav>
                 </div>
                 <div class="header__nav-help hidden md:block ">
-                    <nav class="nav-menu flex-row justify-between ">
+                    <nav class="nav-menu flex-row justify-evenly text-gray-500 [&_ul]:flex [&_ul]:gap-[32px]">
                         <?php wp_nav_menu(array('theme_location' => 'help-menu')); ?>
                     </nav>
                 </div>
--- wp-content/themes/stellar/templates/single-template.php
+++ wp-content/themes/stellar/templates/single-template.php
@@ -1,39 +1,38 @@
 <?php
-    /**
-    * Template Name: Single Template
-    */
-?>  
+/**
+ * Template Name: Single Template
+ */
+?>
 
 <?php get_header(); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php the_content(); ?>
 
-<?php the_content(); ?>
+    <?php
+    $hero = get_field('hero');
+    $image= $hero['left_side']['gallery_cover'];
+    var_dump($image);
+    $size = 'full';
+    if ($hero) : ?>
+        <div id="hero">
+            <div class="left-side">
+            <img src="<?php echo $image; ?>" />
 
+            <p><?php echo ($hero['left_side']['gallery_cover']); ?></p>
+          <?php  echo wp_get_attachment_image( $hero['left_side']['gallery_cover'], $size ); ?>
+            </div>
 
-<?php
-$hero = get_field('hero');
-if ($hero): ?>
-    <div id="hero">
-        <div class="left-side">
-            <img src="<?php echo esc_url($hero['left_side']['gallery_cover']['url']); ?>"
-                 alt="<?php echo esc_attr($hero['left_side']['gallery_cover']['alt']); ?>" />
-
+            <div class="right-side">
+                <h1><?php echo esc_html($hero['right_side']['heading']); ?></h1>
+            </div>
         </div>
-        <div class="right-side">
-            <h1><?php echo esc_html($hero['right_side']['heading']); ?></h1>
-        </div>
-    </div>
-  
+
+    <?php endif; ?>
+
+<?php endwhile; else : ?>
+
 <?php endif; ?>
-
-<?php endwhile;else:?>
-
-
-
-
-<?php  endif; ?>
-
 
 <?php get_footer(); ?>

98ec34395d8b1d2a5ff715fbefbc4924f8831ebd
danic999 <lkordic1999@gmail.com>
2023-10-25 19:37:09 +0200

    sign-in

--- wp-content/themes/stellar/front-page.php
+++ wp-content/themes/stellar/front-page.php
@@ -3,6 +3,8 @@
 <h1><?php the_title(); ?> </h1>
 
 <?php get_template_part('templates/template-parts/section', 'content'); ?>
+<?php get_template_part('templates/sign', 'in'); ?>
+
 
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/stellar/templates/sign-in.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Template Name: Sign in Template
+ */
+?>
+
+<?php wp_head() ?>
+
+
+
+
+<div class="relative flex ">
+    <div class="absolute top-[40px] left-[40px] flex">
+        <img class="inline" src="<?=get_template_directory_uri() . '/assets/images/Frame.png' ?>" alt="">
+        <span class="font-semibold text-[16px]">PNFT Market</span>
+    </div>
+<span class="absolute bottom-[40px] left-[40px]">@Created by NamDesing</span>
+
+    <div class="flex flex-row mx-auto grid md:grid-cols-2">
+        <div class="py-[222px] mx-auto">
+            <div class="flex just flex-col gap-[8px] pr-[19px]">
+                <h1 class="text-[#344054] text-[40px] font-semibold"><?=__('NFT Access','stellar')?></h1>
+                <p class="text-[#667085] text-[16px] "><?=__('Please fill your detail to access your account.','stellar')?></p>
+            </div>
+            <div class="flex flex-col gap-[20px] mt-[56px]">
+                <div class="flex flex-col gap-[8px]"><label class='text-[#344054]' for="email"><?=__('Email','stellar')?></label><input
+                        class="flex px-[14px] py-[10px] border border-solid border-gray-300 rounded-md p-2 text-[#667085] text-[16px]"
+                        name="email" type="mail"> </div>
+                <div class="flex flex-col gap-[8px]"><label class='text-[#344054]' for="password"><?=__('Password','stellar')?></label><input
+                        class="flex px-[14px] py-[10px] border border-solid border-gray-300 rounded-md p-2 text-[#667085] text-[16px]"
+                        name="password" type="password"> </div>
+
+                <div>
+                    <input class='w-[14px] h-[14px] justify-center border-solid border-1 rounded-md mt-[3px] border-[#D0D5DD]' type="checkbox" name="remember" id="">
+                    <label class='font-[500] leading-5 text-[14px]'for="remember"><?=__('Remember me','stellar')?></label> <span class="ml-[113px] leading-5 text-[#5429FF] font-[500] text-[14px]"><?=__('Forgot password?','stellar')?></span>
+                </div>
+            </div>
+            <div class="flex flex-col gap-[20px] mt-[32px] text-center">
+
+                <button class="px-[154px] py-[14px] bg-[#5429FF] text-white rounded-[8px]"><?=__('Sign in','stellar')?></button>
+                <button class="mx-auto border-gray-300 border border-solid rounded-[8px]"><div class='mx-auto my-auto flex flex-row gap-[8px] text-center px-[90px] py-[10px]'><img class='inline'src="<?=get_template_directory_uri() . '/assets/images/Google.png'?>" /><?=__('Sign in with Google','stellar')?></div> </button>
+                <p class="text-[#344054]"><?=__('Don’t have an account?','stellar')?><span class="text-[#5429FF]"><?=__('Sign up','stellar')?></span></p>
+
+
+
+
+            </div>
+        </div>
+        <div class=" hidden md:flex bg-[#CBCCE8] m-[32px] rounded-[32px] h-fit pb-[31px]">
+            <img src="<?=get_template_directory_uri() . '/assets/images/DRIP_20.png' ?>" alt="">
+
+        </div>
+    </div>
+</div>
+
+

a24e5198c4e3b4919cd84c2b88a3c80cf4d6ce2b
danic999 <lkordic1999@gmail.com>
2023-10-27 20:19:56 +0200

    Update sign-in.php

--- wp-content/themes/stellar/templates/sign-in.php
+++ wp-content/themes/stellar/templates/sign-in.php
@@ -19,7 +19,7 @@
 
     <div class="flex flex-col md:flex-row mx-auto">
 
-        <div class="self-center md:flex-1">
+        <div class="self-center md:flex flex-1">
             <div class="max-w-[360px] mx-auto">
                 <div class="flex just flex-col gap-[8px] mb-[56px]">
                     <h1 class="text-[#344054] text-[40px] font-semibold">
@@ -53,30 +53,32 @@
                         </span>
                     </div>
                 </div>
+                <div class="flex flex-col gap-[20px] mt-[32px] text-center">
+
+                    <button class="px-[154px] py-[14px] bg-[#5429FF] text-white rounded-[8px]">
+                        <?= __('Sign in', 'stellar') ?>
+                    </button>
+                    <button class="mx-auto border-gray-300 border border-solid rounded-[8px]">
+                        <div class='mx-auto my-auto flex flex-row gap-[8px] text-center px-[90px] py-[10px]'><img
+                                class='inline'
+                                src="<?= get_template_directory_uri() . '/assets/images/Google.png' ?>" />
+                            <?= __('Sign in with Google', 'stellar') ?>
+                        </div>
+                    </button>
+                    <p class="text-[#344054]">
+                        <?= __('Don’t have an account?', 'stellar') ?><span class="text-[#5429FF]">
+                            <?= __(' Sign up', 'stellar') ?>
+                        </span>
+                    </p>
+
+
+
+
+                </div>
             </div>
-            <div class="flex flex-col gap-[20px] mt-[32px] text-center">
 
-                <button class="px-[154px] py-[14px] bg-[#5429FF] text-white rounded-[8px]">
-                    <?= __('Sign in', 'stellar') ?>
-                </button>
-                <button class="mx-auto border-gray-300 border border-solid rounded-[8px]">
-                    <div class='mx-auto my-auto flex flex-row gap-[8px] text-center px-[90px] py-[10px]'><img
-                            class='inline' src="<?= get_template_directory_uri() . '/assets/images/Google.png' ?>" />
-                        <?= __('Sign in with Google', 'stellar') ?>
-                    </div>
-                </button>
-                <p class="text-[#344054]">
-                    <?= __('Don’t have an account?', 'stellar') ?><span class="text-[#5429FF]">
-                        <?= __(' Sign up', 'stellar') ?>
-                    </span>
-                </p>
-
-
-
-
-            </div>
         </div>
-        <div class=" hidden md:flex bg-[#CBCCE8] m-[32px] rounded-[32px] h-fit pb-[31px]">
+        <div class="hidden md:flex flex-1 bg-[#CBCCE8] m-[32px] rounded-[32px] h-fit pb-[31px]">
             <img src="<?= get_template_directory_uri() . '/assets/images/DRIP_20.png' ?>" alt="">
 
         </div>
